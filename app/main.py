b2cb422f869b34ea1493fdd608a6f9f310822007
David O'Neill <2378524O@student.gla.ac.uk>
2021-06-21 10:33:26 +0100

    First Commit
    Set up basic file structure

---
+++ app/main.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import request
+from operators import Operators
+
+
+app = Flask(__name__)
+operator_util = Operators()
+
+
+@app.route('/mode', methods=['GET'])
+def mode():
+    #return the list of all modes of transport
+    maybe_modes = operator_util.get_modes()
+    if maybe_modes is None:
+        return "Failure", 400
+    return maybe_modes, 200
+
+    
+@app.route('/operator', methods=['GET'])
+def operator():
+    #given the id, return the appropriate operator
+    id = request.args.get('filterString')
+
+    if id is None:
+        #return all operators
+        return 
+
+    maybe_operator = operator_util.get_operator_by_id(id)
+    if maybe_operator is None:
+        return "Failure", 400
+    return maybe_operator, 200
+    
---
+++ app/operators.py
@@ -0,0 +1,15 @@
+class Operators(object):
+    def __init__(self):
+        #google sheet exists which has operator information
+        #use google sheets API to get this spreadsheet information and store it in a list of dictionaries
+        pass
+    
+
+    def get_modes(self):
+        #return a list of dictionaries, each one representing a mode of transport
+        pass
+
+    
+    def get_operator_by_id(self, operator_id):
+        #return a list of dictionaries, each one representing an operator
+        pass
