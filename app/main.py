f0b4cf8c54ece305bd2beb2fb601efb408f37dbc
David O'Neill <2378524O@student.gla.ac.uk>
2021-06-21 16:57:13 +0100

    Updated the app files and also added the openapi.yaml file
    ter the commit message for your changes. Lines starting

--- app/main.py
+++ app/main.py
@@ -6,28 +6,24 @@ from operators import Operators
 app = Flask(__name__)
 operator_util = Operators()
 
-
 @app.route('/mode', methods=['GET'])
 def mode():
-    
-    #return the list of all modes of transport
+    #return all the possible modes of transport
     maybe_modes = operator_util.get_modes()
     if maybe_modes is None:
-        return "Failure", 400
+        return "Error Occurred", 400
     return maybe_modes, 200
 
-    
 @app.route('/operator', methods=['GET'])
 def operator():
     #given the id, return the appropriate operator
     id = request.args.get('filterString')
 
-    if id is None:
-        #return all operators
-        return 
-
     maybe_operator = operator_util.get_operator_by_id(id)
     if maybe_operator is None:
-        return "Failure", 400
+        return "ID not found: %s" %id, 400
     return maybe_operator, 200
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
     
--- app/operators.py
+++ app/operators.py
@@ -1,23 +1,126 @@
 import pandas as pd
-
+import json
 
 class Operators(object):
 
     def __init__(self):
         
-        #use pandas to read the google spreadsheet as a csv file, storing the resulting dataframe as an instance variable
-        sheet_id = "1zjKv2XGf49tSMeUenmY8NrwuSPx5xBbTUclIkf-YDkM"
-        sheet_name = "operator-info"
-        URL = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(sheet_id, sheet_name)
+        #use pandas to read the google spreadsheets as csv files, storing the resulting dataframes as instance variables
+        operator_sheet_id = "1zjKv2XGf49tSMeUenmY8NrwuSPx5xBbTUclIkf-YDkM"
+        operator_sheet_name = "operator-info"
+        operator_URL = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(operator_sheet_id, operator_sheet_name)
+        self.operator_df = pd.read_csv(operator_URL, dtype={'Operator id':str, 'Mode':str})  
 
-        self.df = pd.read_csv(URL, dtype={'Operator id':str, 'Mode':str})  
+        mode_sheet_id = "1p3a4vFTbtY21R0V7cksn-orfAqoSIKKkHLOGrByXSMg"
+        mode_sheet_name = "mode-info"
+        mode_URL = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(mode_sheet_id, mode_sheet_name)
+        self.mode_df = pd.read_csv(mode_URL, dtype={'id':str})
+
+
+    def get_modes(self):
+        return json.dumps(self.mode_df.to_dict("records"))
+
+
+    def populate_json_template(self, operator):
+        fields = ['Operator Description', 'Operator URL (homepage)', 'Operator id', 'Customer Services Contact email', 'Customer Services Contact Phone',
+        'Default Language', 'Number of Modes', 'Mode', 'Operator MIPTA URL']
+
+        template = {
+                "href": "",
+                "item-metadata": [
+                    {
+                        "rel": "urn:X-hypercat:rels:hasDescription:en",
+                        "val": ""
+                    },
+                    {
+                        "rel": "urn:X-hypercat:rels:hasHomepage",
+                        "val": ""
+                    },
+                    {
+                        "rel": "urn:X-opentransport:rels:hasId",
+                        "val": ""
+                    },
+                    {
+                        "rel": "urn:X-opentransport:rels:hasEmail",
+                        "val": ""
+                    },
+                    {
+                        "rel": "urn:X-opentransport:rels:hasPhone",
+                        "val": ""
+                    },
+                    {
+                        "rel": "urn:X-opentransport:rels:hasDefaultLanguage",
+                        "val": ""
+                    },
+                    {
+                        "rel": "urn:X-opentransport:rels:hasNumberModes",
+                        "val": ""
+                    },
+                    {
+                        "rel": "urn:X-opentransport:rels:hasMode1#Code",
+                        "val": ""
+                    },
+                    {
+                        "rel": "urn:X-opentransport:rels:hasMode1#Description",
+                        "val": ""
+                    },
+                    {
+                        "rel": "urn:X-opentransport:rels:hasMIPTAURL",
+                        "val": ""
+                    }
+                ]
+            }
+
+        template['href'] = operator['Open Transport Account API URL']
+
+        for i in range(len(fields)):
+            
+            if template[0]['item-metadata'][i]['rel']!='urn:X-opentransport:rels:hasMode1#Description':
+                template[0]['item-metadata'][i]['val'] = operator[fields[i]]
+            else:
+                template[0]['item-metadata'][i]['val'] = self.mode_df.loc[self.mode_df['id']==operator['Mode']]['short-desc'].iloc[0]
+
+        template[0]['item-metadata'][-1]['val'] = operator['Operator MIPTA URL']
+
+        return template
+
 
     
     def get_operator_by_id(self, operator_id):
         #get row of the dataframe corresponding to the operator_id and return the name of the associated operator
+
+        json_result = [
+                {
+                    "catalogue-metadata": [
+                        {
+                            "rel": "urn:X-hypercat:rels:isContentType",
+                            "val": "application/vnd.hypercat.catalogue+json"
+                        },
+                        {
+                            "rel": "urn:X-hypercat:rels:hasDescription:en",
+                            "val": "OpenTransport Operator Catalogue"
+                        },
+                        {
+                            "rel": "urn:X-hypercat:rels:supportsSearch",
+                            "val": "urn:X-hypercat:search:simple"
+                        }
+                    ],
+                    "items": [
+                        
+                    ]
+                }
+            ]
+            
+
         try:
-            operator_name = self.df.loc[self.df['Operator id']==operator_id]['Operator Description'].iloc[0]
-            return operator_name
+            operator = self.operator_df.loc[self.operator_df['Operator id']==operator_id]
+            operator_info = self.populate_json_template(operator)
+            json_result[0]['items'].append(operator_info)
+            return json_result
+
         except:
-            return None
-        
+            operators = self.operator_df.to_dict("records")
+            for operator in operators:
+                operator_info = self.populate_json_template(operator)
+                json_result[0]['items'].append(operator_info)
+            return json_result
